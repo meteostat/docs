06411bb04bcf55cacee90e691a0eba01e1c184a9
clampr <christian.lamprecht@aol.de>
2025-12-30 19:24:22 +0100

    fix: remove ads from responsive toc

--- src/theme/TOCCollapsible/index.tsx
+++ src/theme/TOCCollapsible/index.tsx
@@ -2,8 +2,6 @@ import React, { type ReactNode } from "react";
 import TOCCollapsible from "@theme-original/TOCCollapsible";
 import type TOCCollapsibleType from "@theme/TOCCollapsible";
 import type { WrapperProps } from "@docusaurus/types";
-import Ad from "@site/src/components/Ad";
-import styles from "./styles.module.css";
 
 type Props = WrapperProps<typeof TOCCollapsibleType>;
 
@@ -11,9 +9,6 @@ export default function TOCCollapsibleWrapper(props: Props): ReactNode {
   return (
     <>
       <TOCCollapsible {...props} />
-      <div className={styles.adWrapper}>
-        <Ad />
-      </div>
     </>
   );
 }
